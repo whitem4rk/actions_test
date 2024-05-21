afa2cc2e4c49c1119fa19f3af49233f59be1d2f8
whitemark <whtmdgus56@nate.com>
2024-05-21 17:35:06 +0900

    init

---
+++ src/test/java/com/example/github_actions/GithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
