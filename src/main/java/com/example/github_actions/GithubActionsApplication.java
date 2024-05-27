083609637aa6784169cb26ac89e11b2ae167c163
scheduled-for-hacker <whtmdgus56@nate.com>
2024-05-27 20:04:35 +0900

    trigger error

--- src/main/java/com/example/github_actions/GithubActionsApplication.java
+++ src/main/java/com/example/github_actions/GithubActionsApplication.java
@@ -8,7 +8,7 @@ public class GithubActionsApplication {
 
   public static void main(String[] args) {
     // trigger error
-    System.out.println(sdf);
+    System.out.println("ROLL BACK");
 
     SpringApplication.run(GithubActionsApplication.class, args);
   }
